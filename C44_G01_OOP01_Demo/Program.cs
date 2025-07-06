6095b066500baa9c8f1a1c151809e99d59970e7b
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-07-06 22:45:43 +0300

    Add project files.

---
+++ C44_G01_OOP01_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+
+namespace C44_G01_OOP01
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+
+            #region Class Library 
+
+            //Product product = new Product();
+            //product.Id = 10;
+            //product.Name = "Test";
+            //product.Price = 1000;
+            //product.Quantit = 1; 
+
+            #endregion
+
+        }
+    }
+}
